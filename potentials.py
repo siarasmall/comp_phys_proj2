2f6add2f6b3fd708c136fb7559683675f23c99ea
siarasmall <41011312+siarasmall@users.noreply.github.com>
2025-02-11 14:39:07 -0500

    Subclass from potential class

--- potentials.py
+++ potentials.py
@@ -1,21 +1,23 @@
 import numpy as np
 
+class Potential:
+    pass
 class FreeParticle:
     pass
 
 class InfiniteSquareWell:
     pass
 
-class FiniteSquareWell:
+class FiniteSquareWell(Potential):
     pass
 
-class TriangleWell:
+class TriangleWell(Potential):
     pass
 
-class SimpleHarmonicOscillator:
+class SimpleHarmonicOscillator(Potential):
     pass
 
-class PotentialBarrier:
+class PotentialBarrier(Potential):
     def __init__(self, start, stop, mag, dim) -> None:
         V = np.zeros([dim, dim])
         for i in range(start, stop):
