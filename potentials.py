5e20680190fcee4d1958a834c2331cc9070167ec
siarasmall <41011312+siarasmall@users.noreply.github.com>
2025-02-13 14:36:45 -0500

    document potentials module

--- potentials.py
+++ potentials.py
@@ -7,6 +7,7 @@ class Potential:
 
         Params:
             - Mags (np array of ints): Magnitudes of x values
+            - Total_num (int): total number of x values
         """
         self.mags = mags
         self.matrix = diags([np.ones(total_num-1),-2*np.ones(total_num),np.ones(total_num-1)], [-1,0,1]).toarray()
@@ -16,7 +17,13 @@ class Potential:
         return self.matrix
 class PotentialWithBarriers(Potential):
     def __init__(self, data, total_num) -> None:
-        # data = list of tuples: (start, stop, mag)
+        """
+        Inits a Potential object with barriers.
+
+        Params:
+            - data: List of tuples (start, stop, mag) representing the start index, stop index, 
+            and magnitude of each "barrier"/block
+        """
         V = np.zeros([total_num, total_num])
         for start, stop, mag in data:
             for i in range (start, stop):
@@ -25,16 +32,45 @@ class PotentialWithBarriers(Potential):
 
 class Barrier(PotentialWithBarriers):
     def __init__(self, start, stop, y0, a, total_num) -> None:
+        """
+        Inits a Potential barrier.
+
+        Params:
+            - Start (int): start index of barrier
+            - Stop (int): stop index of barrier
+            - y0 (int): initial height (pre-barrier)
+            - a (int): height of barrier (NOT total height with barrier)
+            - Total_num (int): total number of x values
+        """
         data = [(0, start, y0), (start, stop, y0 + a), (stop, total_num, y0)]
         super().__init__(data, total_num)
 
 class InfiniteSquareWell(PotentialWithBarriers):
     def __init__(self, start, stop, y0, total_num) -> None:
+        """
+        Inits an infinite square well potential.
+
+        Params:
+            - Start (int): start index of well
+            - Stop (int): stop index of well
+            - y0 (int): initial height (pre-well)
+            - Total_num (int): total number of x values
+        """
         data = [(0, start, y0), (start, stop, -np.inf), (stop, total_num, y0)]
         super().__init__(data, total_num)
 
 class FiniteSquareWell(PotentialWithBarriers):
     def __init__(self, start, stop, y0, a, total_num) -> None:
+        """
+        Inits a finite square well potential.
+
+        Params:
+            - Start (int): start index of well
+            - Stop (int): stop index of well
+            - y0 (int): initial height (pre-well)
+            - a (int): depth of well (NOT total height with well)
+            - Total_num (int): total number of x values
+        """
         data = [(0, start, y0), (start, stop, y0 - a), (stop, total_num, y0)]
         super().__init__(data, total_num)
 
